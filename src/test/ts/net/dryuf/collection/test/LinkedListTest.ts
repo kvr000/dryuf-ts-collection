2177345f7dcf1e0d6690ccd8391ed795a49ed57b
kvr <kvr@centrum.cz>
2016-01-21 03:31:33 +0100

    unit tests dependencies

--- src/test/ts/net/dryuf/collection/test/LinkedListTest.ts
+++ src/test/ts/net/dryuf/collection/test/LinkedListTest.ts
@@ -3,6 +3,17 @@ module net.dryuf.collection.test {
 
 export class LinkedListTest
 {
+	testBasic(): void
+	{
+		var list: LinkedList<number> = new LinkedList<number>();
+		list.add(0);
+		list.add(1);
+		list.add(2);
+		expect(list.get(0)).toBe(0);
+		expect(list.get(1)).toBe(1);
+		expect(list.get(2)).toBe(2);
+	}
+
 }
 
 
